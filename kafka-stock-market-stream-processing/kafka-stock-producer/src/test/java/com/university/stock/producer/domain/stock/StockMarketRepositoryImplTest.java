d511797bbfadffdb707fbab40485c2292672574f
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-03-04 19:07:18 +0100

    Add tests for Stock Repository and Scheduled Producer

---
+++ kafka-stock-market-stream-processing/kafka-stock-producer/src/test/java/com/university/stock/producer/domain/stock/StockMarketRepositoryImplTest.java
@@ -0,0 +1,52 @@
+package com.university.stock.producer.domain.stock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.university.stock.market.model.domain.Stock;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@ExtendWith(MockitoExtension.class)
+class StockMarketRepositoryImplTest {
+
+  @Mock
+  private KafkaTemplate<String, Stock> kafkaTemplate;
+
+  @Mock
+  private NewTopic topic;
+
+  @Captor
+  private ArgumentCaptor<Stock> stockCaptor;
+
+  @InjectMocks
+  private StockMarketRepositoryImpl stockMarketRepository;
+
+  @Test
+  void shouldSendStockToKafka() {
+    //given
+    String topicName = "topic-kafka";
+
+    Stock stock = Stock.builder()
+        .ticker("example-ticker")
+        .build();
+
+    //when
+    when(topic.name()).thenReturn(topicName);
+
+    stockMarketRepository.send(stock);
+
+    //then
+    verify(kafkaTemplate).send(eq(topicName), stockCaptor.capture());
+    assertThat(stockCaptor.getValue()).isEqualTo(stock);
+  }
+}
