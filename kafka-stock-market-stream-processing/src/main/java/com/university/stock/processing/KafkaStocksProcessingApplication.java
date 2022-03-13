9e94389b86d1e0a08f72ca3cec5b9e059fa53777
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-03-13 18:36:42 +0100

    Refactoring - extracting stock producer service from Kafka processing service

--- kafka-stock-market-stream-processing/src/main/java/com/university/stock/processing/KafkaStocksProcessingApplication.java
+++ kafka-stock-market-stream-processing/src/main/java/com/university/stock/processing/KafkaStocksProcessingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StocksProcessingApplication {
+public class KafkaStocksProcessingApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(StocksProcessingApplication.class, args);
+    SpringApplication.run(KafkaStocksProcessingApplication.class, args);
   }
 }
