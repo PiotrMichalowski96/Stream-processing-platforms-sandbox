cd158811c500bc930c0a50eaec8c874911c1612e
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-03-14 19:36:19 +0100

    Kinesis Lambda stock processing module init

---
+++ kinesis-lambda-stock-stream-processing/src/test/java/com/university/kinesis/lambda/stock/stream/processing/KinesisLambdaStockStreamProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.kinesis.lambda.stock.stream.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesisLambdaStockStreamProcessingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
