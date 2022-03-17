d22b505a6e05ae64e25240a3d286595e6b823260
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-03-17 21:12:29 +0100

    Fix Kinesis and Lambda configuration

--- kinesis-lambda-stock-stream-processing/src/main/java/com/university/kinesis/lambda/stock/stream/processing/kinesis/AwsKinesisConfig.java
+++ kinesis-lambda-stock-stream-processing/src/main/java/com/university/kinesis/lambda/stock/stream/processing/kinesis/AwsKinesisConfig.java
@@ -1,7 +1,6 @@
 package com.university.kinesis.lambda.stock.stream.processing.kinesis;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.kinesis.AmazonKinesis;
 import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,20 +10,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsKinesisConfig {
 
-  @Value("${kinesis.aws.access.key}")
-  private String accessKey;
-
-  @Value("${kinesis.aws.secret.key}")
-  private String secretKey;
-
   @Value("${kinesis.aws.region}")
   private String region;
 
   @Bean
   public AmazonKinesis buildAmazonKinesis() {
-    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
     return AmazonKinesisClientBuilder.standard()
-        .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+        .withCredentials(new DefaultAWSCredentialsProviderChain())
         .withRegion(region)
         .build();
   }
