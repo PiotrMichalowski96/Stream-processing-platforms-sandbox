d5ad6170e227851c7bc63ae04a63a11cb54fc0ee
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-03-20 15:30:29 +0100

    Fix timestamp json serialization

--- elasticsearch-stock-consumer/src/main/java/com/university/elasticsearch/stock/consumer/model/StockElasticMessage.java
+++ elasticsearch-stock-consumer/src/main/java/com/university/elasticsearch/stock/consumer/model/StockElasticMessage.java
@@ -1,5 +1,10 @@
 package com.university.elasticsearch.stock.consumer.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +22,9 @@ import lombok.ToString;
 public class StockElasticMessage {
 
   private String id;
+  @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss")
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
   private LocalDateTime timestamp;
   private String streamPlatform;
   private Long processingTimeInMillis;
