249533d198920d2283d0e5f043aa2b84b59e8eda
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-03-22 21:55:59 +0100

    Update Kinesis Listener and add test

--- elasticsearch-stock-consumer/src/main/java/com/university/elasticsearch/stock/consumer/kinesis/KinesisMessageListener.java
+++ elasticsearch-stock-consumer/src/main/java/com/university/elasticsearch/stock/consumer/kinesis/KinesisMessageListener.java
@@ -10,8 +10,6 @@ import com.university.elasticsearch.stock.consumer.mapper.ElasticMessageMapper;
 import com.university.elasticsearch.stock.consumer.service.ElasticProducer;
 import com.university.stock.market.common.util.JsonUtil;
 import com.university.stock.market.model.domain.StockStatus;
-import javax.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -20,30 +18,32 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 @ConditionalOnProperty(value = "consumer.kinesis.enable", havingValue = "true")
 public class KinesisMessageListener {
 
   private static final String SHARD_ID = "shardId-000000000000";
   private static final int REQUEST_LIMIT = 25;
 
-  @Value("${consumer.kinesis.aws.stream}")
-  private final String streamName;
   private final AmazonKinesis kinesis;
-
   private final ElasticProducer elasticProducer;
   private final ElasticMessageMapper elasticMessageMapper;
+  private final GetShardIteratorResult shardIterator;
 
-  private GetShardIteratorResult shardIterator;
+  public KinesisMessageListener(@Value("${consumer.kinesis.aws.stream}") String streamName,
+      AmazonKinesis kinesis,
+      ElasticProducer elasticProducer,
+      ElasticMessageMapper elasticMessageMapper) {
 
-  @PostConstruct
-  private void buildShardIterator() {
+    this.kinesis = kinesis;
+    this.elasticProducer = elasticProducer;
+    this.elasticMessageMapper = elasticMessageMapper;
+
     GetShardIteratorRequest readShardsRequest = new GetShardIteratorRequest()
         .withStreamName(streamName)
         .withShardId(SHARD_ID)
         .withShardIteratorType(ShardIteratorType.LATEST);
 
-    this.shardIterator = kinesis.getShardIterator(readShardsRequest);
+    this.shardIterator = this.kinesis.getShardIterator(readShardsRequest);
   }
 
   @Scheduled(fixedRateString = "${consumer.kinesis.schedule:1000}")
